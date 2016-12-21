1268ca7521b7fea53847abeb525e140063e35fe4
Ross Wardrup <minorsecond@gmail.com>
2016-12-21 14:48:10 -0600

    Example dates and volume for volume graph.

--- app/src/main/java/com/rwardrup/sheiko/MainActivity.java
+++ app/src/main/java/com/rwardrup/sheiko/MainActivity.java
@@ -1,11 +1,17 @@
 package com.rwardrup.sheiko;
 
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.text.ParseException;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,14 +20,95 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // The graph on the main window. See www.android-graphview.org for more info
+        Calendar calendar = Calendar.getInstance();
+
+        // Add example dates
+        String date1 = "01/21/2016";
+        String date2 = "03/21/2016";
+        String date3 = "05/21/2016";
+        String date4 = "07/21/2016";
+        String date5 = "09/21/2016";
+        String date6 = "11/21/2016";
+
+        SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+
+        Date d1 = null;
+        Date d2 = null;
+        Date d3 = null;
+        Date d4 = null;
+        Date d5 = null;
+        Date d6 = null;
+
+        try {
+            d1 = df.parse(date1);
+            calendar.add(Calendar.DATE, 1);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            d2 = df.parse(date2);
+            calendar.add(Calendar.DATE, 1);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            d3 = df.parse(date3);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            d4 = df.parse(date4);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            d5 = df.parse(date5);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            d6 = df.parse(date6);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
         GraphView graph = (GraphView) findViewById(R.id.graph);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
+                //new DataPoint(0, 1),
+                //new DataPoint(1, 5),
+                //new DataPoint(2, 3),
+                //new DataPoint(3, 2),
+                //new DataPoint(4, 6)
+
+                new DataPoint(d1, 25475),
+                new DataPoint(d2, 22330),
+                new DataPoint(d3, 28795),
+                new DataPoint(d4, 29977),
+                new DataPoint(d5, 22590),
+                new DataPoint(d6, 22550)
         });
         graph.addSeries(series);
+
+        // Set the date label formatter
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(MainActivity.this));
+        graph.getGridLabelRenderer().setNumHorizontalLabels(3);
+
+        // Label the y-axis. TODO: let this be set by the user in the settings window.
+        graph.getGridLabelRenderer().setVerticalAxisTitle("Total Lbs.");
+
+        // set manual x bounds to have nice steps
+        //graph.getViewport().setMinX(d1.getTime());
+        //graph.getViewport().setMaxX(d3.getTime());
+        graph.getViewport().setXAxisBoundsManual(true);
+
+        // as we use dates as labels, the human rounding to nice readable numbers
+        // is not necessary
+        graph.getGridLabelRenderer().setHumanRounding(false);
     }
 }
