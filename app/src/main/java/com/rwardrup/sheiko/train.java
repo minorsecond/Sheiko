daf727703fd33375119e0343ced2554c16531446
Ross Wardrup <minorsecond@gmail.com>
2016-12-24 05:22:15 -0600

    Rest timer pause functionality added.

--- app/src/main/java/com/rwardrup/sheiko/train.java
+++ app/src/main/java/com/rwardrup/sheiko/train.java
@@ -3,12 +3,15 @@ package com.rwardrup.sheiko;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class train extends AppCompatActivity {
 
+    public boolean timerIsPaused;
+    public long millisLeftOnTimer;
     Button startBreakTimerButton;
     Button stopBreakTimerButton;
     Button pauseBreakTimerButton;
@@ -31,11 +34,15 @@ public class train extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                breakTimerOutput.setTextSize(31);
-                //countdowntimer = new CountDownTimerClass(180000, 1000);
-                //countdowntimer.start();
+                if (timerIsPaused) {
+                    mCountDownTimer = createTimer(millisLeftOnTimer);  // resume paused timer
 
-                mCountDownTimer = createTimer();
+                    Log.d("resumed timer", "value: " + millisLeftOnTimer);
+                } else {
+                    breakTimerOutput.setTextSize(31);
+                    int timerDuration = 180000;  // This will be set by user in final code
+                    mCountDownTimer = createTimer(timerDuration);
+                }
             }
         });
 
@@ -44,10 +51,10 @@ public class train extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+                timerIsPaused = false;
                 mCountDownTimer.cancel();
                 breakTimerOutput.setTextSize(31);
                 breakTimerOutput.setText("180");
-                //countdowntimer = new CountDownTimerClass(180000, 1000);
             }
         });
 
@@ -56,17 +63,22 @@ public class train extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                countdowntimer.cancel();  // TODO: Fix this
+                timerIsPaused = true;
+                mCountDownTimer.cancel();  // TODO: Fix this
+                Log.d("millis left on pause", "value: " + millisLeftOnTimer);
             }
         });
     }
 
-    private CountDownTimer createTimer() {
-        return new CountDownTimer(180000, 1000) {
+    private CountDownTimer createTimer(long timerDuration) {
+
+        Log.d("new timer duration:", "value: " + timerDuration);
+        return new CountDownTimer(timerDuration, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 int progress = (int) (millisUntilFinished / 1000);
                 breakTimerOutput.setText(Integer.toString(progress));
+                millisLeftOnTimer = millisUntilFinished;
             }
 
             @Override
