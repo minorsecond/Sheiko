3c96879fc9cb3ef032dd65aee00afebb98db60e1
Ross Wardrup <minorsecond@gmail.com>
2016-12-24 04:48:37 -0600

    Added stop button functionality to break timer. Added new resources for large screens (not yet implemented.)

--- app/src/main/java/com/rwardrup/sheiko/train.java
+++ app/src/main/java/com/rwardrup/sheiko/train.java
@@ -9,55 +9,72 @@ import android.widget.TextView;
 
 public class train extends AppCompatActivity {
 
-    Button button;
-    TextView textview;
+    Button startBreakTimerButton;
+    Button stopBreakTimerButton;
+    Button pauseBreakTimerButton;
+    TextView breakTimerOutput;
     CountDownTimer countdowntimer;
+    private CountDownTimer mCountDownTimer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_train);
 
-        button = (Button) findViewById(R.id.startBreakTimer);
-        textview = (TextView) findViewById(R.id.breakTimerOutput);
+        startBreakTimerButton = (Button) findViewById(R.id.startBreakTimer);
+        stopBreakTimerButton = (Button) findViewById(R.id.stopBreakButton);
+        pauseBreakTimerButton = (Button) findViewById(R.id.pauseBreakButton);
+        breakTimerOutput = (TextView) findViewById(R.id.breakTimerOutput);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        // break timer start
+        startBreakTimerButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+                breakTimerOutput.setTextSize(31);
+                //countdowntimer = new CountDownTimerClass(180000, 1000);
+                //countdowntimer.start();
 
-                countdowntimer = new CountDownTimerClass(180000, 1000);
+                mCountDownTimer = createTimer();
+            }
+        });
 
-                countdowntimer.start();
+        // break timer stop
+        stopBreakTimerButton.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                mCountDownTimer.cancel();
+                breakTimerOutput.setTextSize(31);
+                breakTimerOutput.setText("180");
+                //countdowntimer = new CountDownTimerClass(180000, 1000);
+            }
+        });
+
+        // break timer pause
+        pauseBreakTimerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                countdowntimer.cancel();  // TODO: Fix this
             }
         });
     }
 
+    private CountDownTimer createTimer() {
+        return new CountDownTimer(180000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int progress = (int) (millisUntilFinished / 1000);
+                breakTimerOutput.setText(Integer.toString(progress));
+            }
 
-    public class CountDownTimerClass extends CountDownTimer {
-
-        public CountDownTimerClass(long millisInFuture, long countDownInterval) {
-
-            super(millisInFuture, countDownInterval);
-
-        }
-
-        @Override
-        public void onTick(long millisUntilFinished) {
-
-            int progress = (int) (millisUntilFinished / 1000);
-
-            textview.setText(Integer.toString(progress));
-
-        }
-
-        @Override
-        public void onFinish() {
-            textview.setTextSize(18);
-            textview.setText(" Break Over");
-
-        }
+            @Override
+            public void onFinish() {
+                breakTimerOutput.setTextSize(24);
+                breakTimerOutput.setText(" Break Over");
+            }
+        }.start();
     }
-
 
 }
