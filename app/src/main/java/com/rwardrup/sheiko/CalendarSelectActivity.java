1e7133808070200123e3f695b2c1c02849dd2e0d
Ross Wardrup <minorsecond@gmail.com>
2017-01-15 04:03:40 -0600

    Add Jacoco config

--- app/src/main/java/com/rwardrup/sheiko/CalendarSelectActivity.java
+++ app/src/main/java/com/rwardrup/sheiko/CalendarSelectActivity.java
@@ -2,13 +2,19 @@ package com.rwardrup.sheiko;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class CalendarSelectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_history);
+        setContentView(R.layout.activity_select_workout_calendar);
+
+        CalendarView workoutCalendar = (CalendarView) findViewById(R.id.selectWorkoutCalendar);
 
         // TODO: Get workout history & populate calendar with previous & future workouts
         // TODO: Also, show workout summary on bottom portion of activity
